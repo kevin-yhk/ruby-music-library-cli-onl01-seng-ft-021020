5f12ee7cd46045f32c037f52d2b9e4461b432c0b
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-03 02:36:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,5 +47,15 @@ class Song
     @genre.songs << self if !@genre.songs.include?(self)
   end 
   
-  def find_by_name
+  def find_by_name(name)
+    self.all.detect{|song| song.name == name}
+  end 
+
+  def find_or_create_by_name(name)
+    if self.find_by_name(name)
+      return self.find_by_name(name)
+    else 
+      self.create(name)
+    end 
+  end 
 end 

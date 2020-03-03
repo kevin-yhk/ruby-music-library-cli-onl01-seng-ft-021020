8153e8c5eb198e27a2ead83647b8cb171300dcc1
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-03 02:24:19 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,49 @@
+class Song
+  
+  attr_accessor :name
+  attr_reader :artist, :genre
+  
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    if artist != nil 
+      self.artist=artist
+    end 
+
+    if genre != nil 
+      self.genre=genre
+    end 
+    
+    save 
+  end 
+
+  def self.all
+    @@all
+  end 
+
+  def self.destroy_all
+    @@all.clear
+  end 
+
+  def save
+    @@all << self
+  end 
+
+  def self.create(name)
+    song = Song.new(name)
+    song.save
+    song
+  end 
+
+  def artist=(name)
+    @artist = name
+    @artist.add_song(self)
+  end 
+
+  def genre=(genre)
+
+    @genre=genre 
+    @genre.songs << self if !@genre.songs.include?(self)
+  end 
+end 

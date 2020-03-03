c7ddaafdc670e5af7319701e757059868d264499
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-03 02:39:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,44 +1,53 @@
-class Artist 
-  
-  extend Concerns::Findable 
-  attr_accessor :name, :songs 
-  
+class Artist
+  extend Concerns::Findable
+  attr_accessor :name, :artist
+  attr_reader :genre
   @@all = []
-  
+
   def initialize(name)
     @name = name
     @songs = []
-    save
+
   end 
-  
+
   def self.all
-    @@all 
+    @@all
   end 
-  
+
   def self.destroy_all
     @@all.clear
   end 
-  
+
   def save
-    @@all << self 
+    @@all << self
   end 
-  
+
   def self.create(name)
     artist = Artist.new(name)
-    artist.save 
-    artist 
+    artist.save
+    artist
   end 
-  
+
+  def songs 
+    @songs
+  end 
+
   def add_song(song)
-    if song.artist != self 
+    if song.artist== nil
       song.artist = self
     end 
     if !@songs.include?(song)
       @songs << song 
     end 
   end 
-  
+
+
   def genres
-    songs.collect {|song| song.genre}.uniq 
+
+    songs.map{|song| song.genre}.uniq
+
   end 
-end 
+
+
+
+end  

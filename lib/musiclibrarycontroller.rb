2a7e636d48d4fb6d8f257542d5c8a63b1e27550f
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-03 03:27:49 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -7,7 +7,7 @@ class MusicLibraryController
   
   def call 
     input = ""
-    until input = "exit" do 
+    while != "exit" do 
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."

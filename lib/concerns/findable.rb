dcd5f00b3e6d231444fa030318e10c4229d572ae
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-03 02:44:26 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,14 +1,11 @@
-module Concerns::Findable
+module Concerns::Findable 
   def find_by_name(name)
-    self.all.detect{|song| song.name == name}
-  end 
-
+    all.detect{|s| s.name == name}
+  end
+  
   def find_or_create_by_name(name)
-    if self.find_by_name(name)
-      return self.find_by_name(name)
-    else 
-      self.create(name)
-    end 
-  end 
+    find_by_name(name) || create(name)
+  end
 
-end  
+
+end
